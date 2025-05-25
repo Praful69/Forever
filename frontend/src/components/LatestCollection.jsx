db8370a90db840b24dbe2190ee1793d2c7fdc7dd
Praful <prafulrawat2010@gmail.com>
2025-05-25 21:54:48 +0530

    well

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,13 +1,35 @@
-import { useContext } from "react"
+import { useEffect, useState, useContext } from "react"
 import { ShopContext } from "../context/ShopContext"
+import ProductItem from "./ProductItem"  // Changed: removed { } - default import
+import Title from "./Title"              // Changed: removed { } - default import
 
 const LatestCollection = () => {
 
-const {products} = useContext(ShopContext)
-console.log(products)
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, [products])
 
   return (
-    <div>LatestCollection</div>
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab quis, assumenda quae veniam accusantium suscipit architecto perferendis tempore voluptas sit. Amet quas quo debitis sed placeat deserunt cupiditate, saepe omnis!
+        </p>
+      </div>
+      {/*Rendering latest products*/}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((item, index) => (
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+      </div>
+    </div>
   )
 }
+
 export default LatestCollection
